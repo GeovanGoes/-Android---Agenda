7089d4a0bdb1628052cc3f471321cafd48def0d2
GeovanGoes <geovansilvagoes@gmail.com.br>
2018-02-09 17:26:20 -0200

    Adding new course

---
+++ FinancasK/app/src/main/java/br/com/a4kontrol/finanas_k/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,17 @@
+package br.com.a4kontrol.finanas_k.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.com.a4kontrol.finanas_k.R
+
+/**
+ * Created by geovan.goes
+ */
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}

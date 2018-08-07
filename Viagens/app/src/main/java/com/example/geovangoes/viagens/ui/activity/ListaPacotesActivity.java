fcd1fc69892e466dad45476bc1acc9af68f227d6
Geovan Goes <geovangoes@iMac-de-Alessandra-2.local>
2018-08-06 21:16:21 -0300

    starting on constraint leyout

---
+++ Viagens/app/src/main/java/com/example/geovangoes/viagens/ui/activity/ListaPacotesActivity.java
@@ -0,0 +1,17 @@
+package com.example.geovangoes.viagens.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.geovangoes.viagens.R;
+
+public class ListaPacotesActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pacotes);
+    }
+}
